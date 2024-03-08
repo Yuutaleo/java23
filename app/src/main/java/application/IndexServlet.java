6789c8641fc8d2da0289ff6ea0ae2cb29d641448
Yuutaleo <128760920+Yuutaleo@users.noreply.github.com>
2024-03-08 19:33:17 +0000

    aula 3

---
+++ app/src/main/java/application/IndexServlet.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+//@ = annotation
+
+@WebServlet(name = "IndexServlet", urlPatterns = "/") 
+public class IndexServlet extends HttpServlet{
+    public void doGet(
+        HttpServletRequest request,
+        HttpServletResponse response
+    )
+    throws ServletException, IOException{
+        String n = request.getParameter ("nome");
+        request.setAttribute("nome", n);
+        // response.getWriter().print("Hello " + n);
+        request.getRequestDispatcher("/resposta.jsp").forward(request,response);
+
+    }
+
+    
+
+    
+
+}
